11c6ded618d773abadfb9839db2d4fd63ab51c73
GaelGarcia9382 <158115689+GaelGarcia9382@users.noreply.github.com>
2024-05-06 11:53:31 -0600

    Update LibroService.cs
    Ya quedo esta a la verga, creo

--- DataBase/LibroService.cs
+++ DataBase/LibroService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,48 @@ namespace DataBase
 {
     public class LibroService
     {
+         public IList Get(int idAutor)
+ {
+     try
+     {
+         using (var connection = new BibliotecaEntities())
+         {
+             var list = (from a in connection.Libros
+                         where a.IDAutor == idAutor
+                         select a).ToList();
+             return list;
+         }
+     }
+     catch (Exception)
+     {
+         return null;
+     }
+ }
+ public int Insert(int idAutor, string nombre, int disponibilidad,string editorial,date anopubli)
+ {
+     try
+     {
+         using (var connection = new BibliotecaEntities())
+         {
+             Libros nuevolibro = new Libros();
+             nuevolibro.IDAutor = idAutor;
+             nuevolibro.Nombre = nombre;
+             nuevolibro.Disponibilidad = disponibilidad;
+             nuevolibro.Editorial = editorial;
+             nuevolibro.Anopubli = anopubli;
+
+             connection.Libros.Add(nuevolibro);
+
+             connection.SaveChanges();
+
+             return nuevolibro.IDLibro;
+
+         }
+     }
+     catch (Exception)
+     {
+         return 0;
+     }
+ }
     }
 }
