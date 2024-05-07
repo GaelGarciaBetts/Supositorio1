ec94006e8fd63042a5e685112ee168a37a630bb2
GaelGarcia9382 <158115689+GaelGarcia9382@users.noreply.github.com>
2024-05-06 20:05:15 -0600

    Update AutorService.cs
    una raya mas al tirge

--- DataBase/AutorService.cs
+++ DataBase/AutorService.cs
@@ -9,6 +9,44 @@ namespace DataBase
 {
     public class AutorService
     {
-        
+       public IList Get()
+{
+    try
+    {
+        using (var connection = new BibliotecaEntities())
+        {
+            var list = (from a in connection.Autor
+                        select a).ToList();
+            return list;
+        }
+    }
+    catch (Exception) 
+    {
+        return null;
+    }
+}
+public int InsertE(string nombre, string apellido)
+{
+    try
+    {
+        using (var connection = new BibliotecaEntities())
+        {
+            Autor nuevoautor = new Autor();
+            nuevoautor.Nombre = nombre;
+            nuevoautor.Apellido = apellido;
+
+            connection.Autor.Add(nuevoautor);
+
+            connection.SaveChanges();
+
+            return nuevoautor.IDAutor;
+
+        }
+    }
+    catch (Exception)
+    {
+        return 0;
+    }
+} 
     }
 }
